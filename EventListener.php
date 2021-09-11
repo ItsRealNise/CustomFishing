b324b7b64f84be2005fb7a0b8949a6e5a3703733
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2021-09-11 16:03:46 +0700

    Add files via upload

---
+++ EventListener.php
@@ -0,0 +1,77 @@
+<?php
+
+
+declare(strict_types = 1);
+
+// FYI: Event Priorities work this way: LOWEST -> LOW -> NORMAL -> HIGH -> HIGHEST -> MONITOR
+
+namespace YaN;
+
+use pocketmine\block\Block;
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\event\player\{PlayerItemHeldEvent, PlayerLoginEvent, PlayerQuitEvent, PlayerDeathEvent};
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\network\mcpe\protocol\PlayStatusPacket;
+use pocketmine\plugin\Plugin;	
+use pocketmine\Server as PMServer;
+
+class EventListener implements Listener {
+
+	public Plugin $plugin;
+
+	public function __construct(Plugin $plugin){
+		$this->plugin = $plugin;
+	}
+
+	/**
+	 * @param PlayerItemHeldEvent $ev
+	 *
+	 * @priority HIGHEST
+	 * @ignoreCancelled true
+	 */
+	public function onItemHeld(PlayerItemHeldEvent $ev){
+		$session = Fishing::getInstance()->getSessionById($ev->getPlayer()->getId());
+		if($session instanceof Session){
+			if($session->fishing){
+				$inventory = $ev->getPlayer()->getInventory();
+				$oldItem = $inventory->getItemInHand();
+				$newItem = $ev->getItem();
+				if ($oldItem !== $newItem) {
+					$session->unsetFishing();					
+				}
+			}
+		}
+	}
+	
+	public function onDeath(PlayerDeathEvent $ev) {
+		$session = Fishing::getInstance()->getSessionById($ev->getPlayer()->getId());
+		if($session instanceof Session){
+			if($session->fishing){
+				$session->unsetFishing();	
+			}					
+
+		}		
+	}
+
+	
+	/**
+	 * @param PlayerLoginEvent $ev
+	 *
+	 * @priority LOWEST
+	 */
+	public function onLogin(PlayerLoginEvent $ev){
+		Fishing::getInstance()->createSession($ev->getPlayer());
+	}
+	
+	/**
+	 * @param PlayerQuitEvent $ev
+	 *
+	 * @priority LOWEST
+	 */	
+	public function onLeave(PlayerQuitEvent $ev){
+		Fishing::getInstance()->destroySession($ev->getPlayer());
+	}
+}
